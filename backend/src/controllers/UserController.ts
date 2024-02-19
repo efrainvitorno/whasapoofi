bf32f1b7d5eebe6a2f49d1b3e3ba5b2e335df882
efrainvitorno <142545042+efrainvitorno@users.noreply.github.com>
2024-02-19 16:02:44 -0500

    vccv
    cvbv

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -40,7 +40,7 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
     password,
     name,
     profile,
-    isTricked,
+    
     companyId: bodyCompanyId,
     queueIds,
     whatsappId
@@ -66,7 +66,6 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
     password,
     name,
     profile,
-    isTricked,
     companyId: bodyCompanyId || userCompanyId,
     queueIds,
     whatsappId
